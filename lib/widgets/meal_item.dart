148ae6125bcfdd85418e59240d59454122a13e78
Achint-ui <achintharshil@gmail.com>
2022-07-08 13:43:03 +0530

    new

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -31,16 +31,54 @@ class MealItem extends StatelessWidget {
             Stack(
               children: [
                 ClipRRect(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15),
-                      topRight: Radius.circular(15),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(imageUrl,
+                      height: 250, width: double.infinity, fit: BoxFit.cover),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
                     ),
-                    child: Image.network(imageUrl,
-                        height: 250,
-                        width: double.infinity,
-                        fit: BoxFit.cover)),
+                    child: Text(title,
+                        style: TextStyle(fontSize: 26, color: Colors.white),
+                        softWrap: true,
+                        overflow: TextOverflow.fade),
+                  ),
+                )
               ],
-            )
+            ),
+            Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.schedule,
+                        ),
+                        SizedBox(width: 6),
+                        Text('${duration} min'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.work,
+                        ),
+                        SizedBox(width: 6),
+                      ],
+                    )
+                  ],
+                )),
           ],
         ),
       ),
